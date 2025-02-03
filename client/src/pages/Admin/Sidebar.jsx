d707e26e261fdcd5804980aa600f8b307dd64522
arkajyoti-roy <arkajyotiroy445@gmail.com>
2025-02-03 23:19:20 +0530

    update Sidebar.jsx and CourseTab.jsx

--- client/src/pages/Admin/Sidebar.jsx
+++ client/src/pages/Admin/Sidebar.jsx
@@ -6,8 +6,8 @@ import { ChartNoAxesColumn, SquareLibrary } from "lucide-react";
 const Sidebar = () => {
   return (
     <>
-     <div className="flex mt-12">
-      <div className="hidden lg:block w-[250px] sm:w-[300px] space-y-8 border-r border-gray-300 dark:border-gray-700  p-5 sticky top-0  h-screen">
+     <div className="flex mt-12  top-0">
+      <div className="hidden lg:block w-[250px] sm:w-[300px] space-y-8 border-r border-gray-300 dark:border-gray-700  p-5   h-screen">
         <div className="space-y-4 ">
           <Link to="dashboard" className="flex items-center gap-2">
             <ChartNoAxesColumn size={22} />
--- client/src/pages/Admin/course/CourseTab.jsx
+++ client/src/pages/Admin/course/CourseTab.jsx
@@ -10,9 +10,18 @@ import {
 import { Input } from "@/Components/ui/input";
 import RichTextEditor from "@/Components/RichTextEditor";
 import { useState } from "react";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/Components/ui/select";
+import { useNavigate } from "react-router-dom";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/Components/ui/select";
+import { Loader2 } from "lucide-react";
 
 const CourseTab = () => {
+  const isLoading = false;
   const [input, setInput] = useState({
     courseTitle: "",
     subTitle: "",
@@ -24,10 +33,37 @@ const CourseTab = () => {
   });
 
   const changeEventHandler = (e) => {
-    const { name, value } = e.targer;
+    const { name, value } = e.target;
     setInput({ ...input, [name]: value });
+    console.log(setInput);
   };
 
+  const selectCategory = (value) => {
+    setInput({ ...input, category: value });
+  };
+
+  const selectCourseLevel = (value) => {
+    setInput({ ...input, courseLevel: value });
+  };
+
+  const [previewThumbnail, setPreviewThumbnail] = useState("");
+
+  const selectThumbnail = (e) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setInput({ ...input, courseThumbnail: file });
+      const fileReader = new FileReader();
+      fileReader.onloadend = () => {
+        setPreviewThumbnail(fileReader.result);
+      };
+      fileReader.readAsDataURL(file);
+    }
+  };
+const updateCourseHandler = () =>{
+    console.log(input);
+    
+}
+  const navigate = useNavigate();
   const isPublished = true;
   return (
     <Card>
@@ -75,54 +111,78 @@ const CourseTab = () => {
           <div className="flex items-center gap-5">
             <div>
               <Label>Category</Label>
-              <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select a category" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="Web Development">Web Development</SelectItem>
-              <SelectItem value="DSA">DSA</SelectItem>
-              <SelectItem value="Cloud">Cloud</SelectItem>
-              <SelectItem value="Networking">Networking</SelectItem>
-              <SelectItem value="DBMS">DBMS</SelectItem>
-              <SelectItem value="Other">Other</SelectItem>
-            </SelectContent>
-          </Select>
+              <Select onValueChange={selectCategory}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="Web Development">
+                    Web Development
+                  </SelectItem>
+                  <SelectItem value="DSA">DSA</SelectItem>
+                  <SelectItem value="Cloud">Cloud</SelectItem>
+                  <SelectItem value="Networking">Networking</SelectItem>
+                  <SelectItem value="DBMS">DBMS</SelectItem>
+                  <SelectItem value="Other">Other</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
-          
-          <div>
-            <Label>Course Level</Label>
-            <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select a course level" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="Beginner">Beginner</SelectItem>
-              <SelectItem value="Medium">Medium</SelectItem>
-              <SelectItem value="Advance">Advance</SelectItem>
-             
-            </SelectContent>
-          </Select>
-          </div>
-          <div>
-            <Label>Price in (INR)</Label>
-            <Input
-            type="number"
-            name="coursePrice"
-            value={input.coursePrice}
-            onChange={changeEventHandler}
-            placeholder="ex. 133"
-            className="w-fit"
-            />
-          </div>
+
+            <div>
+              <Label>Course Level</Label>
+              <Select onValueChange={selectCourseLevel}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a course level" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="Beginner">Beginner</SelectItem>
+                  <SelectItem value="Medium">Medium</SelectItem>
+                  <SelectItem value="Advance">Advance</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label>Price in (INR)</Label>
+              <Input
+                type="number"
+                name="coursePrice"
+                value={input.coursePrice}
+                onChange={changeEventHandler}
+                placeholder="ex. 133"
+                className="w-fit"
+              />
+            </div>
           </div>
           <div>
             <Label>Course Thumbnail</Label>
             <Input
-            type="file"
-            accept="image/*"
-            className="w-fit"
+              type="file"
+              accept="image/*"
+              onChange={selectThumbnail}
+              className="w-fit"
             />
+            {previewThumbnail && (
+              <img
+                src={previewThumbnail}
+                className="w-64 my-2"
+                alt="course thumbnail"
+              />
+            )}
+          </div>
+          <div className="space-x-2">
+            <Button onClick={() => navigate("/admin/course")} varient="outline">
+              Cancel
+            </Button>
+            <Button onClick={updateCourseHandler} className="" disabled={isLoading}>
+              {isLoading ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4" animate-spin /> Please
+                  wait...
+                </>
+              ) : (
+                "Save"
+              )}
+            </Button>
           </div>
         </div>
       </CardContent>
