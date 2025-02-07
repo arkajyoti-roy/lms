85c40d529cda36d4a7fec9c93ea1de18224e869d
arkajyoti-roy <arkajyotiroy445@gmail.com>
2025-02-07 23:36:11 +0530

    update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,11 +10,8 @@ import { ToastContainer } from "react-toastify";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={appStore}>
-   
       <App />
-    
-      <ToastContainer />
-      
+      <ToastContainer position="bottom-right" />
     </Provider>
   </React.StrictMode>
 );
