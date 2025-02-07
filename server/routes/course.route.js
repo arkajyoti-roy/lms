c5043c2918ba76333f7eb62986c9bf56ab640bef
arkajyoti-roy <arkajyotiroy445@gmail.com>
2025-02-07 23:07:24 +0530

    update course.route.js

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -5,27 +5,18 @@ import upload from '../utils/multer.js'; // Correct import path
 
 const router = express.Router();
 
-// Course Routes
-router.route('/')
-  .post(isAuthenticated, createCourse);
+router.route('/').post(isAuthenticated, createCourse);
+router.route('/getCourses').get(isAuthenticated, getCreatorCourses);
+router.route('/:courseId').put(isAuthenticated, upload.single("courseThumbnail"), editCourse);
+router.route('/:courseId').get(isAuthenticated, getCourseById);
+router.route('/:courseId/lecture').post(isAuthenticated, createLecture);
+router.route('/:courseId/lecture').get(isAuthenticated, getCourseLecture);
 
-router.route('/getCourses')
-  .get(isAuthenticated, getCreatorCourses);
 
-router.route('/:courseId')
-  .put(isAuthenticated, upload.single("courseThumbnail"), editCourse)
-  .get(isAuthenticated, getCourseById);
+router.route('/:courseId/lecture/:lectureId').post(isAuthenticated, editLecture);
+router.route('/lecture/:lectureId').delete(isAuthenticated, removeLecture);
+router.route('/lecture/:lectureId').get(isAuthenticated, getLectureById);
 
-// Lecture Routes
-router.route('/:courseId/lecture')
-  .post(isAuthenticated, createLecture)
-  .get(isAuthenticated, getCourseLecture);
 
-router.route('/:courseId/lecture/:lectureId')
-  .post(isAuthenticated, editLecture);
-
-router.route('/lecture/:lectureId')
-  .delete(isAuthenticated, removeLecture)
-  .get(isAuthenticated, getLectureById);
 
 export default router;
