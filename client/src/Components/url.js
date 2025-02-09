078797b6d2fca4afb66fe3abd4af449971e5c144
arkajyoti-roy <arkajyotiroy445@gmail.com>
2025-02-09 19:54:15 +0530

    update url.js

--- client/src/Components/url.js
+++ client/src/Components/url.js
@@ -1,6 +1,6 @@
 // src/urls.js
-
-export const BASE_URL = "http://localhost:8081/api/v1";
+export const MAIN_URL = "http://localhost:8081";
+export const BASE_URL = `${MAIN_URL}/api/v1`;
 export const COURSES_URL = `${BASE_URL}/course`;
 export const LOGIN_URL = `${BASE_URL}/auth/login`;
 export const MEDIA_URL = `${BASE_URL}/media`;
